97df7073f8846ba41d0835818ab7ce17fb783463
andimascode <78234026+andimascode@users.noreply.github.com>
2022-02-21 18:01:21 +0100

    master

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  content: ['./layouts/*.html'],
+  theme: {
+    extend: {
+      translate:{
+68:'17.5rem',
+      },
+      scale: { 
+      60:'0.6',
+      },
+      height: {
+100: '53.125rem',
+86:'24.5rem',
+81:'21.5rem',
+97:'28rem',
+      },
+      width: {
+        100:'46.875rem',
+        86:'21rem',
+        99.5:'37rem',
+        97:'28rem',
+      },
+      fontFamily : {
+        poppins: 'Poppins'
+      },
+      colors: {
+        textColor: '#2B2945',
+        gradientGreen : '#B6F3E0',
+        green:'#1FC28B',
+        lightGreen : '#F7FDFB',
+        white: '#EDFDF7 ',
+        originalWhite: '#FFFFFF',
+        orange: '#FFAA82',
+        lightOrange: '#FFF4EF',
+        grayColor : '#8A9DA2',
+        orangeIcon:'#FF894E',
+        section5 : '#E8FCF6',
+        
+      }
+    },
+  },
+  plugins: [],
+}
